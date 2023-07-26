26453ea3d7dac230e6212a6a05678249b35c3367
Azalea <me@hydev.org>
2023-07-25 20:33:15 -0700

    [O] Custom ^C message

--- hyfetch/__main__.py
+++ hyfetch/__main__.py
@@ -1,4 +1,9 @@
-from hyfetch import main
+from . import main
+from .color_util import printc
 
 if __name__ == '__main__':
-    main.run()
+    try:
+        main.run()
+    except KeyboardInterrupt:
+        printc('&cThe program is interrupted by ^C, exiting...')
+        exit(0)
